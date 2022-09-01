a7640e6cc591743592199345b6dae87cfea01607
Anubhav Kumar <anubhavcse08@users.noreply.github.com>
2022-09-01 14:05:07 +0530

    Alignment and forecast status impl

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SplineChart from "./components/SplineChart";
 import Header from "./components/header/header";
 import Form from "./components/Form/form";
 import { header } from "./apiData/header";
+import TitleForecastStatus from "./components/dashboard/TitleForecastStatus";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -122,7 +123,11 @@ class App extends Component {
               </div>
             </div>
             <div class="card-body py-3 px-4">
+              <TitleForecastStatus />
               <TableDesc />
+              <div className="flex flex-row justify-center">
+                <button className="py-1.5 px-3.5 font-medium text-sky-700 border-2 border-sky-700 rounded-lg">Load More</button>
+              </div>
             </div>
           </div>
         </section>
