fab27b1089d8ac2ca10e439def5303d393a4f470
Ranadheer Goud <ranadheer.goud.moonraft@aegonlife.com>
2022-09-01 18:45:41 +0530

    Updated source design & json

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# abs_poc_moonraft
+## ABS Metrcis POC - React
+
+This is a POC for creating a webpage in React highlighting Metrics for each quarter & weekly Forecast details showing multiple statuses for each vendor. This project is built with React, Javascript, Tailwind CSS and Highcharts. 
+
+Note: Webpage renders content from static JSON present in apiData folder.
+
+## Project Status
+
+This project is currently in development. Have added multiple placeholders which doesn't have any functionality.
+
+## Project Screen Shot(s)
+
+Please find the below PDF link to access the Webpage design.
+
+## Installation and Setup Instructions
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`  
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+`localhost:3000/`  
--- src/apiData/forecastData.js
+++ src/apiData/forecastData.js
@@ -2,93 +2,197 @@ export const foreCastInfo = [
     {
         week: "10/03/22",
         weekNumber: 40,
+        quarter: "Q4, 2022",
         target: "$800.87",
         targetType: "Good"
     },
     {
         week: "10/10/22",
         weekNumber: 41,
+        quarter: "Q4, 2022",
         target: "$801.87",
         targetType: "Good"
     },
     {
         week: "10/17/22",
         weekNumber: 42,
+        quarter: "Q4, 2022",
         target: "$810.87",
         targetType: "Good"
     },
     {
         week: "10/24/22",
         weekNumber: 43,
+        quarter: "Q4, 2022",
         target: "$520.87",
         targetType: "Average"
-    }, {
+    }, 
+    {
         week: "10/31/22",
         weekNumber: 44,
+        quarter: "Q4, 2022",
         target: "$802.87",
         targetType: "Good"
     },
     {
         week: "11/07/22",
         weekNumber: 45,
+        quarter: "Q4, 2022",
         target: "$500.87",
         targetType: "Average"
     },
     {
         week: "11/14/22",
         weekNumber: 46,
+        quarter: "Q4, 2022",
         target: "$700.87",
         targetType: "Good"
     },
     {
         week: "11/21/22",
         weekNumber: 47,
+        quarter: "Q4, 2022",
         target: "$500.87",
         targetType: "Average"
     },
     {
         week: "11/28/22",
         weekNumber: 48,
+        quarter: "Q4, 2022",
         target: "$200.87",
         targetType: "Low"
     },
     {
         week: "12/05/22",
         weekNumber: 49,
+        quarter: "Q4, 2022",
         target: "$507.87",
         targetType: "Average"
     },
     {
         week: "12/12/22",
         weekNumber: 50,
+        quarter: "Q4, 2022",
         target: "$501.87",
         targetType: "Average"
     },
     {
         week: "12/19/22",
         weekNumber: 51,
+        quarter: "Q4, 2022",
         target: "$202.87",
         targetType: "Low"
     },
     {
         week: "12/26/22",
         weekNumber: 52,
+        quarter: "Q4, 2022",
         target: "$102.87",
         targetType: "Low"
     },
-
+    {
+        week: "01/03/23",
+        weekNumber: 53,
+        quarter: "Q4, 2022",
+        target: "$800.87",
+        targetType: "Good"
+    },
+    {
+        week: "01/10/23",
+        weekNumber: 54,
+        quarter: "Q4, 2022",
+        target: "$801.87",
+        targetType: "Good"
+    },
+    {
+        week: "01/17/23",
+        weekNumber: 55,
+        quarter: "Q4, 2022",
+        target: "$510.87",
+        targetType: "Average"
+    },
+    {
+        week: "01/24/23",
+        weekNumber: 56,
+        quarter: "Q4, 2022",
+        target: "$220.87",
+        targetType: "Low"
+    }, 
+    {
+        week: "01/31/23",
+        weekNumber: 57,
+        quarter: "Q4, 2022",
+        target: "$802.87",
+        targetType: "Good"
+    },
+    {
+        week: "02/07/23",
+        weekNumber: 58,
+        quarter: "Q4, 2022",
+        target: "$200.87",
+        targetType: "Low"
+    },
+    {
+        week: "02/14/23",
+        weekNumber: 58,
+        quarter: "Q4, 2022",
+        target: "$500.87",
+        targetType: "Average"
+    },
+    {
+        week: "02/21/23",
+        weekNumber: 59,
+        quarter: "Q4, 2022",
+        target: "$500.87",
+        targetType: "Average"
+    },
+    {
+        week: "02/28/23",
+        weekNumber: 60,
+        quarter: "Q4, 2022",
+        target: "$800.87",
+        targetType: "Good"
+    },
+    {
+        week: "03/05/23",
+        weekNumber: 61,
+        quarter: "Q4, 2022",
+        target: "$507.87",
+        targetType: "Average"
+    },
+    {
+        week: "03/12/23",
+        weekNumber: 62,
+        quarter: "Q4, 2022",
+        target: "$201.87",
+        targetType: "Low"
+    },
+    {
+        week: "03/19/23",
+        weekNumber: 63,
+        quarter: "Q4, 2022",
+        target: "$502.87",
+        targetType: "Average"
+    },
+    {
+        week: "03/26/23",
+        weekNumber: 64,
+        quarter: "Q4, 2022",
+        target: "$802.87",
+        targetType: "Good"
+    },
 ]
 
 export const weekWiseData = {
-    weekStart: "10/03/22",
-    quarter: "Q4, 2022",
+    weekStart: "10/03/22",   
     target: "$8,081.00",
     changeInPercent: "3.1",
     isTrendPositive: false,
     data: [
         {
-            Title: "Lipton Pure Leaf Tea 64..",
+            Title: "Lipton Pure Leaf Tea 641231-1231 Limited",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -100,6 +204,7 @@ export const weekWiseData = {
         {
             Title: "Intelligentsia 12oz-181850-25",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -118,6 +223,7 @@ export const weekWiseData = {
         {
             Title: "STARBUCK COFFEE",
             noOfRows: 2,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -136,6 +242,7 @@ export const weekWiseData = {
         {
             Title: "Starbucks K-Cups...",
             noOfRows: 2,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -161,6 +268,7 @@ export const weekWiseData = {
         {
             Title: "Chameleon Cold Brew...",
             noOfRows: 2,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -200,6 +308,7 @@ export const weekWiseData = {
         {
             Title: "NESCAFE Clasico-638264-25",
             noOfRows: 2,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -225,6 +334,7 @@ export const weekWiseData = {
         {
             Title: "bai 18 FZ-180458-25",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -236,6 +346,7 @@ export const weekWiseData = {
         {
             Title: "OVALTINE RICH",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -247,6 +358,7 @@ export const weekWiseData = {
         {
             Title: "Peet's Bags 12oz-182363-25",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/24/22",
                 Forecast: "$20.99",
@@ -258,6 +370,7 @@ export const weekWiseData = {
         {
             Title: "Peet's Bags 18oz-188340-25",
             noOfRows: 1,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -269,6 +382,7 @@ export const weekWiseData = {
         {
             Title: "KGM K-Cups 12ct-182671-25",
             noOfRows: 3,
+            quarter: "Q4, 2022",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -306,8 +420,28 @@ export const weekWiseData = {
             }]
         },
         {
+            Title: "Brookebond 3 Roses Coffee",
+            noOfRows: 3,
+            quarter: "Q4, 2022",
+            forecastInfo: [{
+                startWeek: "10/03/22",
+                Forecast: "$20.99",
+                status: "Approved",
+                weeks: 13,
+                $row: 1
+            },
+            {
+                startWeek: "10/03/22",
+                Forecast: "$12.99",
+                status: "Draft",
+                weeks: 1,
+                $row: 2
+            }]
+        },
+        {
             Title: "Lipton Pure - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -319,6 +453,7 @@ export const weekWiseData = {
         {
             Title: "Intelligentsia - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -337,6 +472,7 @@ export const weekWiseData = {
         {
             Title: "STARBUCK COFFEE - 2",
             noOfRows: 2,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -355,6 +491,7 @@ export const weekWiseData = {
         {
             Title: "Starbucks K-Cups - 2",
             noOfRows: 2,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -380,6 +517,7 @@ export const weekWiseData = {
         {
             Title: "Chameleon Cold Brew - 2...",
             noOfRows: 2,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -419,6 +557,7 @@ export const weekWiseData = {
         {
             Title: "NESCAFE Clasico-638264-25 - 2",
             noOfRows: 2,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -444,6 +583,7 @@ export const weekWiseData = {
         {
             Title: "bai 18 FZ-180458-25 - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -455,6 +595,7 @@ export const weekWiseData = {
         {
             Title: "OVALTINE RICH - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$12.99",
@@ -466,6 +607,7 @@ export const weekWiseData = {
         {
             Title: "Peet's Bags 12oz-182363-25 - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/24/22",
                 Forecast: "$20.99",
@@ -477,6 +619,7 @@ export const weekWiseData = {
         {
             Title: "Peet's Bags 18oz-188340-25 - 2",
             noOfRows: 1,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -488,6 +631,7 @@ export const weekWiseData = {
         {
             Title: "KGM K-Cups 12ct-182671-25 - 2",
             noOfRows: 3,
+            quarter: "Q1, 2023",
             forecastInfo: [{
                 startWeek: "10/03/22",
                 Forecast: "$20.99",
@@ -523,6 +667,25 @@ export const weekWiseData = {
                 weeks: 4,
                 $row: 3
             }]
-        }
+        },
+        {
+            Title: "Brookebond 3 Roses Coffee",
+            noOfRows: 3,
+            quarter: "Q1, 2023",
+            forecastInfo: [{
+                startWeek: "10/03/22",
+                Forecast: "$20.99",
+                status: "Approved",
+                weeks: 13,
+                $row: 1
+            },
+            {
+                startWeek: "10/03/22",
+                Forecast: "$12.99",
+                status: "Draft",
+                weeks: 1,
+                $row: 2
+            }]
+        },
     ]
 }
